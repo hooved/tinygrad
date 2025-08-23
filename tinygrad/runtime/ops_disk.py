7b3479b8499c4e8bd2d20f240e3dce27d90611b7
hooved <172129504+hooved@users.noreply.github.com>
2025-08-23 13:46:36 +0000

    allow read-only loading

--- tinygrad/runtime/ops_disk.py
+++ tinygrad/runtime/ops_disk.py
@@ -30,7 +30,11 @@ class DiskDevice(Compiled):
       os.close(fd)
     else:
       try: self.fd = os.open(filename, os.O_RDWR|os.O_CREAT|getattr(os, "O_DIRECT", 0))
-      except OSError: self.fd = os.open(filename, os.O_RDWR|os.O_CREAT)
+      except OSError:
+        try:
+          self.fd = os.open(filename, os.O_RDWR|os.O_CREAT)
+        except PermissionError: # when loading weights from a read-only path, above throws exception
+          self.fd = os.open(filename, os.O_RDONLY)
       if not pathlib.Path(filename).is_block_device() and os.fstat(self.fd).st_size < self.size: os.ftruncate(self.fd, self.size)
       self.mem = mmap.mmap(self.fd, self.size)
     if hasattr(self.mem, 'madvise') and (hp := getattr(mmap, "MADV_HUGEPAGE", None)) is not None:
