edac6835883f28d6866b50d3368ef6e4fac898dd
hooved <172129504+hooved@users.noreply.github.com>
2025-09-06 17:19:54 +0000

    eval original v-pred model

--- examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_red/dev_run.sh
+++ examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_red/dev_run.sh
@@ -67,11 +67,12 @@ export UNET_CKPTDIR="${BASEDIR}/checkpoints/training_checkpoints/${DATETIME}"
 mkdir -p $UNET_CKPTDIR
 export RUN_EVAL=1
 export EVAL_ONLY=1
-export EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/09050413/run_eval_original"
-#export KEEP_EVAL_CACHE=1
+#export EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/09050413/run_eval_original"
+export EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/09050413/run_eval_original_v"
+export KEEP_EVAL_CACHE=1
 #export EVAL_OVERFIT_SET=1
 #export EVAL_INTERVAL=2065
-#export LIMIT_EVAL_SAMPLES=600
+export LIMIT_EVAL_SAMPLES=600
 
 # mi300x
 export DATADIR="/raid/datasets/stable_diffusion"
@@ -82,8 +83,8 @@ export CKPTDIR="/raid/weights/stable_diffusion"
 #export CKPTDIR="/home/hooved/stable_diffusion/checkpoints"
 
 #export WANDB=1
-export PARALLEL=4
-#export PARALLEL=0
+#export PARALLEL=4
+export PARALLEL=0
 
 #EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/09022307/run_eval_762" RUNMLPERF=1 python3 examples/mlperf/model_train.py
 RUNMLPERF=1 python3 examples/mlperf/model_train.py
