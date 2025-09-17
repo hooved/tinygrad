a6f7b2a986d15c735af17c0f17aa1a579fbd387f
hooved <172129504+hooved@users.noreply.github.com>
2025-09-17 14:35:52 +0000

    load vae and verify eval

--- examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_8xMI300X/dev_run.sh
+++ examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_8xMI300X/dev_run.sh
@@ -29,7 +29,12 @@ mkdir -p $UNET_CKPTDIR
 #export RESUME_ITR=15240
 
 for i in {0..7}; do sudo rocm-smi -d $i --setperfdeterminism 1500; done
-sudo rocm-smi -d 0 1 2 3 4 5 6 7 --setpoweroverdrive 450
-TOTAL_CKPTS=10 LEARNING_RATE="2.5e-7" RUNMLPERF=1 python3 examples/mlperf/model_train.py && \
-sudo rocm-smi -d 0 1 2 3 4 5 6 7 --setpoweroverdrive 750 && \
-EVAL_CKPT_DIR=$UNET_CKPTDIR python3 examples/mlperf/model_eval.py
+sudo rocm-smi -d 0 1 2 3 4 5 6 7 --setpoweroverdrive 750
+EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/09100305/run_eval_15240" python3 examples/mlperf/model_eval.py
+
+#for i in {0..7}; do sudo rocm-smi -d $i --setperfdeterminism 1500; done
+#sudo rocm-smi -d 0 1 2 3 4 5 6 7 --setpoweroverdrive 450
+#TOTAL_CKPTS=10 LEARNING_RATE="2.5e-7" RUNMLPERF=1 python3 examples/mlperf/model_train.py
+#TOTAL_CKPTS=10 LEARNING_RATE="2.5e-7" RUNMLPERF=1 python3 examples/mlperf/model_train.py && \
+#sudo rocm-smi -d 0 1 2 3 4 5 6 7 --setpoweroverdrive 750 && \
+#EVAL_CKPT_DIR=$UNET_CKPTDIR python3 examples/mlperf/model_eval.py
